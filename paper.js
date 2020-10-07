edb87042bdd7d97f724700320ba54a851f48d49e
srisahiti <71134221+srisahiti@users.noreply.github.com>
2020-10-07 15:51:38 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class paper{
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.image=loadImage("images/paper.png");
+        this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+        World.add(world,this.body);
+    }
+    display(){
+
+        var posi = this.body.position;
+
+        push();
+        translate(posi.x,posi.y);
+        imageMode(CENTER);
+        strokeWeight(3);
+        fill(255,0,255);
+        //ellipse(0,0,this.r,this.r);
+        image(this.image,0,0,this.r,this.r);
+        pop();
+    }
+}
